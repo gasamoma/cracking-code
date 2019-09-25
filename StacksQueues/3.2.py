1c345af0287fd82fd9f0832f1138382df8ca82f2
Gabriel <gabriel@mocreditomovil.com>
2019-09-24 21:53:12 -0500

    making it more complicated lol

--- StacksQueues/3.2.py
+++ StacksQueues/3.2.py
@@ -4,35 +4,54 @@
 #only positive values? A:NO
 # the entire stack is the same data type? A:yes
 # do i return the min elem index or value A: value
-class classic_stack:
+class NClassicStack:
   def __init__(self):
-     self.stack=[]
-  def push(self, value):#O(1)
-    self.stack.insert(0,value)
-    return True
-  def pop(self):#O(1)
-    if len(self.stack) == 0:
+    self.head = None
+    self.next = None
+    self.value = None
+  def push(self, value):
+    new_head = NClassicStack()
+    new_head.value=value
+    new_head.next = self.head
+    self.head = new_head
+  def pop(self):
+    if self.head is None:
       return None
-    return self.stack.pop(0)
-  def peek(self):#O(1)
-    if len(self.stack) == 0:
+    value = self.head.value
+    self.head = self.head.next
+    return value
+  def peek(self):
+    if self.head is None:
       return None
-    return self.stack[0]
+    return self.head.value
+  def is_empty(self):
+    if self.head is None:
+      return True
+    return False
+  def print_stack(self):
+    if self.head is None:
+      return
+    if self.head.next is None:
+      print(self.value, end='')
+      return
+    print(self.head.value, end='')
+    return self.head.next.print_stack
+
 class stack:
   def __init__(self):
-     self.stack=[]
-     self.mini=classic_stack()
+     self.stack=NClassicStack()
+     self.mini=NClassicStack()
   def push(self, value):#O(1)
     if self.mini.peek() is None or self.mini.peek() >= value:
       self.mini.push(value)
-    self.stack.insert(0,value)
+    self.stack.push(value)
     return True
   def pop(self):#O(1)
-    if len(self.stack) == 0:
+    if self.stack.is_empty():
       return None
-    if self.mini.peek()==self.stack[0]:
+    if self.mini.peek()==self.stack.peek():
       self.mini.pop()
-    return self.stack.pop(0)
+    return self.stack.pop()
   def min(self):#O(1)
     return self.mini.peek()
 
@@ -45,11 +64,9 @@ st.push(8)
 st.push(0)
 st.push(-50)
 st.push(-20)
-print(st.stack) # [-20, -50, 0, 8, 2, 3, 3, 6]
-print(st.mini.stack) # [-50, 0, 2, 3, 3, 6]
 print(st.pop())#-20
 print(st.pop())#-50
 print(st.pop())#0
+print(st.pop())#8
 print(st.min())#2
-print(st.stack) # [8, 2, 3, 3, 6]
 #when you dont know the answer use yourself in a simpler version XD
